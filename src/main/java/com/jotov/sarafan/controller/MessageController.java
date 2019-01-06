e47b021cba2f0431a8728b62bc16499dc5c67494
Vladimir Jotov <v.jotov@gmail.com>
2019-01-07 00:04:38 +0700

    first commit

---
+++ src/main/java/com/jotov/sarafan/controller/MessageController.java
@@ -0,0 +1,14 @@
+package com.jotov.sarafan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    @GetMapping
+    public String list() {
+        return "index";
+    }
+}
