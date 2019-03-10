05c7bdb045906c3918b6520a7870ddf51480a39a
Vladimir Jotov <v.jotov@gmail.com>
2019-03-10 21:11:23 +0700

    comments - domain

--- src/main/java/com/jotov/sarafan/domain/User.java
+++ src/main/java/com/jotov/sarafan/domain/User.java
@@ -1,6 +1,7 @@
 package com.jotov.sarafan.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -13,8 +14,11 @@ import java.time.LocalDateTime;
 @Data
 public class User implements Serializable {
     @Id
+    @JsonView(Views.IdName.class)
     private String id;
+    @JsonView(Views.IdName.class)
     private String name;
+    @JsonView(Views.IdName.class)
     private String userpic;
     private String email;
     private String gender;
